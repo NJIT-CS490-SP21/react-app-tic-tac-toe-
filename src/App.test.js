e49607870de7a1d6c3377779b03cbe236c3a8f8f
gabin ntankeu <gabinho770@yahoo.fr>
2021-03-15 02:43:42 +0000

    added tests for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen,fireEvent,userEvent,type } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("Show login button", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const login_button = screen.getByText('Log in');
+  expect(login_button).toBeInTheDocument();
+  fireEvent.change(screen.getByRole('textbox'), { target: { value: 'hey' } });
+  fireEvent.click(login_button);
+  expect(login_button).not.toBeInTheDocument();
+});
+test("Show leaderboard", () => {
+  render(<App />);
+  const login_button = screen.getByText('Log in');
+  //userEvent.type(screen.getByRole('textbox'), 'Hello, World!');
+  fireEvent.change(screen.getByRole('textbox'), { target: { value: 'hey' } });
+  fireEvent.click(login_button);
+  const show_rank_button = screen.getByText('Show rank');
+  expect(show_rank_button).toBeInTheDocument();
+  fireEvent.click(show_rank_button);
+  const search_button = screen.getByText('Search');
+  expect(search_button).toBeInTheDocument();
+});
+test("Show tchat", () => {
+  render(<App />);
+  fireEvent.change(screen.getByRole('textbox'), { target: { value: 'hey' } });
+  const login_button = screen.getByText('Log in');
+  fireEvent.click(login_button);
+  const tchat_button = screen.getByText('Post');
+  expect(tchat_button).toBeInTheDocument();
+  fireEvent.change(screen.getByRole('textbox'), { target: { value: 'hey' } });
+  const text = screen.getByText('hey');
+  expect(text).toBeInTheDocument();
 });
